f6a0f151ec9bd08209f88757923851f6c7c20d41
ben <noahbenaas@gmail.com>
2021-07-12 12:49:18 +0200

    organized the app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill" />
+        repos
+      </h1>
+    </div>
+  );
+}
+
+export default App;
